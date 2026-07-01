cca306b22769d03637a8679cf64f3284b1a8eaf6
bestgamer123-5 <gamingforevertogether@gmail.com>
2026-07-01 15:27:38 +0200

    kk

--- src/main/java/com/carlib/api/CarIntegrationProvider.java
+++
@@ -1,9 +0,0 @@
-package com.carlib.api;
-
-import java.util.Set;
-
-public interface CarIntegrationProvider {
-    String providerId();
-    Set<String> supportedFeatures();
-    Object invoke(String featureKey, Object input);
-}
